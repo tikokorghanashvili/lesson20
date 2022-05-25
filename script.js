9623f0dc8aed1843e1fe58039097e580ff69d7c1
tikokorghanashvili <101791266+tikokorghanashvili@users.noreply.github.com>
2022-05-25 18:17:41 +0400

    add

--- script.js
+++ script.js
@@ -5,4 +5,22 @@ for (let i=0; i<accordion.length; i++){
     accordion[i].addEventListener('click',function(){
         this.classList.toggle('active');
     })
-}
+};
+
+
+let navigation=document.getElementById('navbarlinks');
+let toggleButton=document.getElementById('burger');
+
+toggleButton.addEventListener('click',function(){
+    navigation.classList.toggle('activeNavigation');
+});
+
+
+let burger=document.querySelectorAll('.burger');
+let burgerbar1=document.querySelectorAll('.ul');
+
+burger.addEventListener('click',()=>{
+    burger.classList.toggle('active');
+    burgerbar1.classList.toggle('active');
+});
+
